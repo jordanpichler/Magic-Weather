9705c9ecddbeb57602f1f3b338b07655990b044a
Jordan Pichler <jordan2084@gmail.com>
2018-03-02 18:04:06 +0100

    - add Rain animation xib with UIView class

---
+++ Magic Weather/RainCloud.swift
@@ -0,0 +1,16 @@
+//
+//  RainCloud.swift
+//  Magic Weather
+//
+//  Created by Jordan Pichler on 20/02/2018.
+//  Copyright © 2018 Jordan A. Pichler. All rights reserved.
+//
+
+import UIKit
+
+class RainCloud: UIView {
+    @IBOutlet weak var cloud: UIImageView!
+    @IBOutlet weak var dropletsTop: UIImageView!
+    @IBOutlet weak var dropletsCenter: UIImageView!
+    @IBOutlet weak var dropletsBottom: UIImageView!
+}
